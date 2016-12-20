64dfbfaafb55edb9ec3dd05c90d07f58a452c652
Nvzgul <h3rmess@gmail.com>
2016-12-21 00:28:42 +0300

    -ADDED COMMENTS

--- sol.py
+++ sol.py
@@ -1,10 +1,11 @@
+#ТАБЛИЦА ИЗ УСЛОВИЯ ЗАДАЧИ
 data = [{"electorate": 3100, "Money": 1050000},
         {"electorate": 2600, "Money": 750000},
         {"electorate": 3500, "Money": 1200000},
         {"electorate": 2800, "Money": 900000},
         {"electorate": 2400, "Money": 600000}]
 
-
+#МЕТОД НАХОЖДЕНИЯ МИНИМАЛЬНОЙ СУММЫ НАШИХ ДАННЫХ
 def my_min(arr):
     min = data[0]['Money']
     for i in arr:
@@ -12,6 +13,7 @@ def my_min(arr):
             min = i['Money']
     return min
 
+#МЕТОД НАХОЖДЕНИЯ МАКСИМАЛЬНОГО КОЛИЧЕСТВА ГОЛОСОВ ИЗ НАШИХ ДАННЫХ
 def max_v(arr):
     max = 0
     for i in arr:
@@ -23,6 +25,8 @@ def max_v(arr):
 
 res = []
 
+#ОСНОВНОЙ МЕТОД, НАХОДЯЩИЙ ОПТИМАЛЬНОЕ ПО ЦЕНЕ РЕШЕНИЕ.
+#РЕАЛИЗОВАН В ВИДЕ РЕКУРСИИ
 def find_max(x,cash = 3000000):
     max = 0
     for i in x:
